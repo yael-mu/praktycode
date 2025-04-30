72d2683e61d1121726ff8668ee593e6d28da9ec9
yael munsa <munsayael@gmail.com>
2025-04-30 23:15:22 +0300

    df

--- TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9d85ea0520194d9b91d83aa61bb555088c184ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fd9b4a75a96d9cec7a45ec21f943ed06c0161db")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
