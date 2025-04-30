6fd9b4a75a96d9cec7a45ec21f943ed06c0161db
yael munsa <munsayael@gmail.com>
2025-04-30 22:43:54 +0300

    create node express

--- TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee5973a730a981423b574a2aa612c3586d9ed4cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9d85ea0520194d9b91d83aa61bb555088c184ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
